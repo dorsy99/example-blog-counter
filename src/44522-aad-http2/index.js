103351593921f31720e47bd694647b852157fe63
Andrew Albury-Dor <dorsy99@gmail.com>
2020-03-31 12:55:00 +1000

    subcom showing, attempting OOB httpEffect

--- src/44522-aad-http2/index.js
+++ src/44522-aad-http2/index.js
@@ -1,20 +1,54 @@
 import {createCustomElement} from '@servicenow/ui-core';
 import '@servicenow/now-button';
+import {createHttpEffect} from '@servicenow/ui-effect-http';
 import snabbdom from '@servicenow/ui-renderer-snabbdom';
 import styles from './styles.scss';
 
-const view = (state, {dispatch, updateState}) => {
-    <div>
-        <h1>test</h1>
-    </div>
+const INC_FETCH_SUCCESS = 'INC_FETCH_SUCCESS'
+
+const fetchIncEffect = createHttpEffect(':url', {
+    method: 'GET',
+    headers: {},
+    pathParams: ['url'],
+    dataParam: 'data',
+    successActionType: INC_FETCH_SUCCESS
+});
+
+const handleFetchIncSucceeded = ({action, updateState}) => {
+	var x = action.payload;
+	console.log(x);
+	/* updateState({
+		path: "inc",
+		value: x,
+		operation: "set"
+	})*/ 
 }
+
+const view = (state, {dispatch, updateState}) => {
+	function getIncident() {
+		dispatch('INC_FETCHED', {url: '/api/now/table/incident?sysparm_limit=1'});
+	}
+
+    return(
+        <now-button 
+            cta="Positive" 
+            iconName="" 
+            iconSet="solid" 
+            label="Fetch an INC" 
+            size="md" 
+            variant="primary" 
+            on-click={getIncident}
+        />
+
+    );
+};
 
 createCustomElement('x-44522-aad-http2', {
 	renderer: { type: snabbdom },
-	initialState: {
-	},
-	actionHandlers: {
-	},
-	view,
+    view,
+    actionHandlers: {
+        'INC_FETCHED': fetchIncEffect,
+        'INC_FETCH_SUCCESS': handleFetchIncSucceeded,
+    },
 	styles
 });
